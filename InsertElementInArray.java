d5e835bd56d2e2eceb172a394d3337d9584e19b5
ajayg138 <ajaygole728@gmail.com>
2024-10-21 02:45:34 +0530

    InsertElementInArray

---
+++ InsertElementInArray.java
@@ -0,0 +1,16 @@
+public class InsertElementInArray{
+
+    public static void insert(int[] arr,int pos){
+
+        
+
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        int n=arr.length;
+
+
+        int pos=n+1;
+
+    }
+}
