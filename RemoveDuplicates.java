ae406834b6698a92bd3d7bd68e069ef40c007cf1
ajayg138 <ajaygole728@gmail.com>
2024-10-22 18:22:27 +0530

    UniqueCount

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,4 +1,21 @@
 public class RemoveDuplicates{
+
+    public static int count(int[] arr){
+        int n=arr.length;
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[j] != arr[i]){
+                j++;
+                arr[j]=arr[i];
+            }
+        }
+        return j+1;
+    }
+
     public static void main(String[] args) {
-        
+        int[] arr={1,1,2,2,3,3,4,4};
+
+        int res=count(arr);
+        System.out.println("Number of unique elements present in array: "+res);
     }}
