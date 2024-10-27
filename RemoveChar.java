d6f2606efb41f24f798d74f96bd42bdf7228b8f6
ajayg138 <ajaygole728@gmail.com>
2024-10-27 11:52:16 +0530

    RemoveChar

---
+++ RemoveChar.java
@@ -0,0 +1,8 @@
+public class RemoveChar{
+
+    public static void main(String[] args){
+        String str="AjayGole";
+        str=str.replace("Gole","");
+        System.out.println(str);
+    }
+}
