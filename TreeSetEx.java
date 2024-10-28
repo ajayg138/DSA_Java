9d86eafacc82b902e6a79dff1a679541dbcdb0cc
ajayg138 <ajaygole728@gmail.com>
2024-10-28 16:20:30 +0530

    TreeSet

---
+++ TreeSetEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class TreeSetEx{
+
+    public static void main(String[] args){
+
+        TreeSet ts=new TreeSet();
+        ts.add(30);
+        ts.add(40);
+        ts.add(60);
+        ts.add(10);
+        ts.add(20);
+        ts.add(50);
+
+        System.out.println(ts);             //Follows sorting order......
+    }
+}  
