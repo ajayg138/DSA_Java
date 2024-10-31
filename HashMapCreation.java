42657e584fd94e47ec23032ee01fd8c9557e511f
ajayg138 <ajaygole728@gmail.com>
2024-10-31 23:43:55 +0530

    HashMap

---
+++ HashMapCreation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HashMapCreation{
+    public static void main(String[] args){
+
+    // HashMap<Integer,String> hm=new HashMap<>();
+
+    //     hm.put(1,"Ajay");
+    //     hm.put(2,"Pravin");
+    //     hm.put(3,"Onkar");
+    //     hm.put(4,"Amol");
+    //     hm.put(5,"Nagesh");
+    //     hm.put(6,"Roshan");
+    //     hm.put(7,"Chetan");
+
+
+    //     System.out.print(hm);
+
+    System.out.println("Hello");
+    
+}
+}
