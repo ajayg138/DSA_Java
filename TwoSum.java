a9c07587450beef8b9a722108f4ad73f7331880c
ajayg138 <ajaygole728@gmail.com>
2024-10-31 23:25:04 +0530

    TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -1,20 +1,41 @@
 
-import java.util.Arrays;
+import java.util.*;
 public class TwoSum{
 
     public static int[] twosum(int[] arr,int target){
+        // int n=arr.length;
+
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[i]+arr[j]==target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+
+
+
+        HashMap<Integer,Integer> hm=new HashMap<>();
+
         int n=arr.length;
 
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j]==target){
-                    return new int[]{i,j};
-                }
+        for(int i=0;i<n;i++){
+            hm.put(arr[i],i);
+        }
+
+        for(int i=0;i<n;i++){
+            int complement=target-arr[i];
+            if(hm.containsKey(complement) && hm.get(complement) != i){
+                return new int[]{hm.get(complement),i};
             }
         }
+
         return new int[]{};
     }
-    public static void main(String[] args){
+        
+
+public static void main(String[] args){
         int[] arr={1,2,3,4,5,5,6};
         int target=10;
 
