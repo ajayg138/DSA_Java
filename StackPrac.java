d24ea970d12c3f983aac96766acd74afa56f372c
ajayg138 <ajaygole728@gmail.com>
2024-11-01 23:09:38 +0530

    StackUpdate

--- StackPrac.java
+++ StackPrac.java
@@ -7,7 +7,6 @@ public class StackPrac{
         Stack<Integer> st=new Stack<>();
 
         st.push(10);
-        st.push(20);
         st.push(30);
         st.push(40);
         System.out.print(st);
@@ -17,6 +16,14 @@ public class StackPrac{
         System.out.print(st);
         System.out.print(st.size());
         System.out.print(st);
+
+        st.push(60);
+        st.push(90);
+        st.push(80);
+        st.push(70);
+
+        System.out.print(st);
+                System.out.print(st.peak());
 
     }
 }
